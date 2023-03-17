bd9832d629e5296a8d91356bd433955b897c517b
bmargon <bmargon@DAW1-33.dip.local>
2023-03-17 12:10:37 +0100

    s

--- Chess/src/BoardLayer/Position.java
+++ Chess/src/BoardLayer/Position.java
@@ -10,6 +10,34 @@ public class Position {
     private int row;
     private int column;
 
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    @Override
+    public String toString(){
+        //falta seguir por implementar eso.
+    return row + ", " + column;
+    }
+    
     public void setValues(int column, int row) {
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
 }
